64c6854a4f3a0bd5dea0bc5f97f4ef951055ec03
stinymac <757462339@qq.com>
2019-10-23 22:29:32 +0800

    data structures - queue

--- data-structures-and-algorithms/data-structures/src/main/java/org/mac/explorations/algs/ds/queue/Main.java
+++ data-structures-and-algorithms/data-structures/src/main/java/org/mac/explorations/algs/ds/queue/Main.java
@@ -34,7 +34,7 @@ public class Main {
 
             System.out.println(arrayQueue);
         }*/
-/*
+        /*
         Queue<Integer> circularQueue = new CircularQueue<>();
 
         for (int i = 0 ; i < 50; i++) {
